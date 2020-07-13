1c5f50812233b9443b681e5019e84af6fd81b0a1
Gio <giovanni.berl1@gmail.com>
2020-07-13 22:43:14 +0200

    devi creare i posts

---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.dashboard')
+
+@section('content')
+
+<div class="container-fluid">
+    <row>
+        <div class="col-12">
+            <h1>
+                hai fatto il login nella dashboard
+            </h1>
+        </div>
+    </row>
+</div>
+
+@endsection
